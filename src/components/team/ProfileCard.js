74804898e69202cee4cf098aa18a50fa369da205
Shivam Bhosle <shivambhosle270903@gmail.com>
2023-12-30 18:54:35 +0530

    Team Images Added

--- src/components/team/ProfileCard.js
+++ src/components/team/ProfileCard.js
@@ -19,26 +19,27 @@ function ProfileCard({ data }) {
           <Card.Text>{data.role}</Card.Text>
           <Col className="profile-social-media">
             <ul className="list-unstyled mx-auto">
-              <li className="list-inline-item">
-                <a href={data.twitter} target="_blank" rel="noreferrer">
-                  <FontAwesomeIcon icon={faXTwitter} />
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a href={data.linkedin} target="_blank" rel="noreferrer">
-                  <FontAwesomeIcon icon={faLinkedin} />
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a href={data.github} target="_blank" rel="noreferrer">
-                  <FontAwesomeIcon icon={faGithub} />
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a href={data.portfolio} target="_blank" rel="noreferrer">
-                  <FontAwesomeIcon icon={faGlobeAsia} />
-                </a>
-              </li>
+              {data.linkedin && (
+                <li className="list-inline-item">
+                  <a href={data.linkedin} target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon icon={faLinkedin} />
+                  </a>
+                </li>
+              )}
+              {data.github && (
+                <li className="list-inline-item">
+                  <a href={data.github} target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon icon={faGithub} />
+                  </a>
+                </li>
+              )}
+              {data.portfolio && (
+                <li className="list-inline-item">
+                  <a href={data.portfolio} target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon icon={faGlobeAsia} />
+                  </a>
+                </li>
+              )}
             </ul>
           </Col>
         </Card.Body>
